c63b2436f3362fd8962383fc69aee8ae757c6526
Martin Sanne Kristiansen <martin@heydays.no>
2018-10-03 17:02:37 +0200

    move feed to context

--- src/components/utilities/Context.js
+++ src/components/utilities/Context.js
@@ -1,11 +1,15 @@
 import React, { Component, createContext } from 'react'
-import { getNavMenu } from '../../utils/wp'
+import { getNavMenu, getPosts } from '../../utils/wp'
 const MyContext = createContext()
 
 export class Provider extends Component {
   state = {
     showMenu: false,
-    menuItems: []
+    menuItems: [],
+    posts: [],
+    postsPage: 1,
+    loadingNext: false,
+    allPagesLoaded: false
   }
 
   componentDidMount = () => {
@@ -16,6 +20,12 @@ export class Provider extends Component {
         })
       })
       .catch(err => console.log(err))
+
+    getPosts(this.state.postsPage).then(res => {
+      this.setState({
+        posts: res
+      })
+    })
   }
 
   toggleMenu = () => {
@@ -23,6 +33,28 @@ export class Provider extends Component {
       prevState => ({ showMenu: !prevState.showMenu }),
       () => this.noScroll()
     )
+  }
+
+  nextPage = () => {
+    const nextPage = this.state.postsPage + 1
+    this.setState({ loadingNext: true })
+    getPosts(nextPage)
+      .then(res => {
+        this.setState(prevState => {
+          return {
+            posts: [...prevState.posts, ...res],
+            postsPage: nextPage,
+            loadingNext: false
+          }
+        })
+      })
+      .catch(err => {
+        if (err && err.code && err.code === 'rest_post_invalid_page_number') {
+          this.setState({
+            allPagesLoaded: true
+          })
+        }
+      })
   }
 
   noScroll = () => {
@@ -37,7 +69,8 @@ export class Provider extends Component {
         value={{
           state: this.state,
           actions: {
-            toggleMenu: this.toggleMenu
+            toggleMenu: this.toggleMenu,
+            nextPage: this.nextPage
           }
         }}
       >
--- src/containers/FrontPage.js
+++ src/containers/FrontPage.js
@@ -1,84 +1,46 @@
 import React, { Component } from 'react'
-import { getPosts } from '../utils/wp'
 import Article from '../containers/Article'
 import uuid from 'uuid/v1'
 import cc from 'classcat'
-import Loading from '../components/Loading'
 import StickyPosts from '../components/StickyPosts'
+import { Consumer } from '../components/utilities'
 
 class FrontPage extends Component {
-  state = {
-    posts: [],
-    page: 1,
-    loadingNext: false,
-    allPagesLoaded: false,
-    loading: true
-  }
-
-  componentDidMount = () => {
-    getPosts(this.state.page).then(res => {
-      this.setState({
-        posts: res,
-        loading: false
-      })
-    })
-  }
-
-  loadNextPage = () => {
-    const nextPage = this.state.page + 1
-    this.setState({ loadingNext: true })
-    getPosts(nextPage)
-      .then(res => {
-        this.setState(prevState => {
-          return {
-            posts: [...prevState.posts, ...res],
-            page: nextPage,
-            loadingNext: false
-          }
-        })
-      })
-      .catch(err => {
-        if (err && err.code && err.code === 'rest_post_invalid_page_number') {
-          this.setState({
-            allPagesLoaded: true
-          })
-        }
-      })
-  }
-
   render() {
-    const { posts, loadingNext, allPagesLoaded, loading } = this.state
-    if (loading) {
-      return <Loading />
-    }
     return (
-      <article>
-        <StickyPosts />
-        <div className="Articles">
-          {posts &&
-            posts.map(p => (
-              <div key={uuid('post')} className="Articles__item">
-                <Article post={p} />
-              </div>
-            ))}
-        </div>
-        <div className="MoreArticles">
-          {!allPagesLoaded && (
-            <button
-              className={cc({
-                MoreArticles__button: true,
-                button: true,
-                'button--loading': loadingNext
-              })}
-              disabled={loadingNext}
-              onClick={this.loadNextPage}
-            >
-              {loadingNext ? 'Laster innlegg' : 'Last flere innlegg'}
-            </button>
-          )}
-          {allPagesLoaded && <div>Alle innlegg lastet</div>}
-        </div>
-      </article>
+      <Consumer>
+        {ctx => (
+          <article>
+            <StickyPosts />
+            <div className="Articles">
+              {ctx.state.posts &&
+                ctx.state.posts.map(p => (
+                  <div key={uuid('post')} className="Articles__item">
+                    <Article post={p} />
+                  </div>
+                ))}
+            </div>
+            <div className="MoreArticles">
+              {!ctx.state.allPagesLoaded && (
+                <button
+                  className={cc({
+                    MoreArticles__button: true,
+                    button: true,
+                    'button--loading': ctx.state.loadingNext
+                  })}
+                  disabled={ctx.state.loadingNext}
+                  onClick={ctx.actions.nextPage}
+                >
+                  {ctx.state.loadingNext
+                    ? 'Laster innlegg'
+                    : 'Last flere innlegg'}
+                </button>
+              )}
+              {ctx.state.allPagesLoaded && <div>Alle innlegg lastet</div>}
+            </div>
+          </article>
+        )}
+      </Consumer>
     )
   }
 }
